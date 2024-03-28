effe969251cd21d6b40671f7c7d8db6eda0d0843
Aleksandr <admin@ijo42.ru>
2024-03-28 17:02:47 +0500

    perf: output optimize

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
@@ -114,27 +115,26 @@ void printMatrix(const SparseDoubleLinkedMatrix& matrix) {
     std::ranges::copy(matrix.columnPointer, columnTail.begin());
     for (const auto lineHead: matrix.linePointer) {
         auto lineTail = lineHead;
-        for (int i = 0; i < columnTail.size(); i++) {
-            if(lineTail && lineTail == columnTail.at(i)) {
-                printf("%6.1f", columnTail.at(i)->value);
+        for (auto & i : columnTail) {
+            if(lineTail && lineTail == i) {
+                std::cout << std::format("{:6.2f}", i->value);
                 lineTail = lineTail->nextLine;
-                columnTail.at(i) = columnTail.at(i)->nextColumn;
-            }else
-                printf("%6.1f", 0.0);
+                i = i->nextColumn;
+            } else
+                std::cout << std::format("{:6.0f}", 0.0);
         }
-        printf("\n");
+        std::cout << "\n";
     }
 }
 
 SparseDoubleLinkedMatrix loadFromFile(const std::string &path) {
     std::ifstream input(path);
     std::string lineString;
-    std::vector<std::string> line;
     SparseDoubleLinkedMatrix matrix{};
     std::unordered_map<unsigned int, SparseDoubleLinkedMatrixElement*> elements{};
 
     std::getline(input,lineString);
-    line = split(lineString, DLSMDelimiter);
+    std::vector<std::string> line = split(lineString, DLSMDelimiter);
 
     loadPointers(line, elements, matrix.linePointer);    // загрузка указателей на строки
 
@@ -150,13 +150,12 @@ SparseDoubleLinkedMatrix loadFromFile(const std::string &path) {
     return matrix;
 }
 
-std::string join(const std::vector<unsigned int> &lst, const char delim)
-{
+std::string join(const std::vector<unsigned int> &lst, const char delim) {
     std::string ret;
     for(const auto &s : lst) {
         if(!ret.empty())
             ret += delim;
-        ret += std::to_string(s);
+        ret.append(std::to_string(s));
     }
     return ret;
 }
@@ -170,16 +169,16 @@ unsigned int findElementNext(std::vector<SparseDoubleLinkedMatrixElement *>::ite
 void saveToFile(const std::string &path, const SparseDoubleLinkedMatrix& matrix) {
     std::vector<SparseDoubleLinkedMatrixElement*> elements(matrix.linePointer.size()*matrix.columnPointer.size() / 10);
     std::vector<unsigned int> lineIds(matrix.linePointer.size(), 0), columnIds(matrix.columnPointer.size(), 0);
-    unsigned int k = 1, i = 0, j;
+    unsigned int k = 1, i = 0;
 
     std::vector<SparseDoubleLinkedMatrixElement*> columnPointer(matrix.columnPointer.size());
-    std::copy(matrix.columnPointer.begin(), matrix.columnPointer.end(), columnPointer.begin());
+    std::ranges::copy(matrix.columnPointer, columnPointer.begin());
     for(auto lineHead = matrix.linePointer.begin(); lineHead != matrix.linePointer.end(); ++i, ++lineHead){
         auto lineTail = *lineHead;
-        j = 0;
+        unsigned int j = 0;
         for (auto columnHead = columnPointer.begin(); columnHead != columnPointer.end(); ++j, ++columnHead) {
             if(lineTail && lineTail == *columnHead) {
-                elements.push_back(*columnHead);
+                elements[k-1] = (*columnHead);
                 lineTail = lineTail->nextLine;
                 *columnHead = (*columnHead)->nextColumn;
                 if(columnIds[j] == 0)
@@ -191,23 +190,31 @@ void saveToFile(const std::string &path, const SparseDoubleLinkedMatrix& matrix)
         }
     }
 
+    elements.resize(k-1);
+    auto outputText = std::vector<std::string>(elements.size() + 2);
 
-    /* TODO: оптимизировать вывод */
-    output << join(columnIds, DLSMDelimiter) << "\n"
-            << join(lineIds, DLSMDelimiter) << "\n";
+    outputText[0] = join(columnIds, DLSMDelimiter);
+    outputText[1] =  join(lineIds, DLSMDelimiter);
+
     k = 1;
     for (auto current = elements.begin(); current != elements.end(); ++current, ++k) {
-        output << (*current)->value << DLSMDelimiter <<
-            ((*current)->nextLine ? k+1 : 0)
-                                         << DLSMDelimiter <<
-            ((*current)->nextColumn ? k + findElementNext(current, (*current)->nextColumn) : 0)
-                                         << "\n";
+        outputText[k+1] = std::format("{:.2f};{};{}",
+            (*current)->value,
+            (*current)->nextLine ? k+1 : 0,
+            (*current)->nextColumn ? k + findElementNext(current, (*current)->nextColumn) : 0
+        );
     }
+
+    std::ofstream output(path, std::ios::trunc);
+    std::ranges::copy(outputText, std::ostream_iterator<std::string>(output, "\n"));
 }
 
 
 int main() {
     auto matrix = loadFromFile(R"(..\examples\ex1.txt)");
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     printMatrix(matrix);
     saveToFile(R"(..\examples\ex0.txt)", matrix);
 
