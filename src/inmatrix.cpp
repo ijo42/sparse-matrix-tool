2a6775ac3a42a627f7e0b086c5bef2c6f85d9a20
Aleksandr <admin@ijo42.ru>
2024-04-11 11:28:51 +0500

    chore: add matrix improve

--- src/inmatrix.cpp
+++ src/inmatrix.cpp
@@ -1,6 +1,12 @@
 #include "../headers/inmatrix.h"
+#include "io.h"
 #include "ui_inmatrix.h"
 #include "../headers/creatematrix.h"
+#include "QFileDialog"
+
+#include <headers/explorer.h>
+#include <headers/preview.h>
+#include <headers/warring.h>
 
 inmatrix::inmatrix(QWidget *parent)
     : QDialog(parent)
@@ -21,9 +27,40 @@ void inmatrix::on_pushButton_2_clicked()//открыть окно создани
     w3.exec();
 }
 
+QString getLastSubstringOrLastFive(const QString &input) {
+    // Проверяем, содержит ли строка символ "/"
+    int lastSlashIndex = input.lastIndexOf('/');
+    if (lastSlashIndex != -1) {
+        // Если содержит, возвращаем подстроку после последнего символа "/"
+        return input.mid(lastSlashIndex + 1);
+    } else {
+        // Если не содержит, возвращаем последние 5 символов
+        if (input.length() > 5) {
+            return input.right(5);
+        } else {
+            // Если строка короче 5 символов, возвращаем всю строку
+            return input;
+        }
+    }
+}
 
 void inmatrix::on_pushButton_clicked()//открыть окно импорта
 {
-
+    QString filename = QFileDialog::getOpenFileName(this, "Выберите файл матрицы", "", "*.dlsm");
+    if(filename.isEmpty())
+        return;
+    bool isSuccess = false;
+    std::string path = filename.toStdString();
+    auto matrix = loadFromFileValidate(isSuccess, path);
+    if(isSuccess){
+        matrix->name = getLastSubstringOrLastFive(filename).toStdString();
+        explorer::getMatrixs().append(matrix);
+        Preview p = Preview(matrix, this);
+        p.show();
+    } else  {
+        warring w;
+        w.setModal(true);
+        w.exec();
+    }
 }
 
